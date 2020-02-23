e9e19551203e3dcac8c5bf9afb9589dabe86c32d
Poonam <poonamverma90@ymail.com>
2020-02-23 16:32:29 +0530

    Add files

---
+++ config.json.py
@@ -0,0 +1,25 @@
+{
+    "params":
+        {
+            "local_server":"True",
+            "local_uri":"mysql://root:@localhost/codingthunder",
+            "prod_uri":"mysql://root:@localhost/codingthunder",
+            "fb_url":"https://facebook.com/codingthunder",
+            "tw_url":"https://twitter.com/codingthunder",
+            "gh_url":"https://github.com/codingthunder",
+            "blog_name":"Coding Thunder",
+            "tag_line":"A Blog liked by Programmers",
+            "gmail_user":"",
+            "gmail_password":"",
+            "about_text":"Hi my name is Poonam and I am a Python Developer",
+            "no_of_posts":2,
+            "admin_user":"poonam",
+            "admin_password":"poonam123",
+            "upload_location":"C:\\Users\\poonam\\PycharmProjects\\Flask-Project1"
+
+
+
+
+        }
+
+}
